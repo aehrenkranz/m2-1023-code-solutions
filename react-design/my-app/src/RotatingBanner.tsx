f585d6462851c68993884c1463d2bd153a11ca81
Alex Ehrenkranz <alex.ehrenkranz+git@gmail.com>
2024-01-02 23:17:12 +0000

    updated typescript, and event handler for indices function

--- react-design/my-app/src/RotatingBanner.tsx
+++ react-design/my-app/src/RotatingBanner.tsx
@@ -1,37 +1,57 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import './RotatingBanner.css';
-
 type Props = {
   items: string[];
-  currentIndex: number;
+};
+type ButtonProps = {
   onCustomClick: () => void;
 };
+type BannerProps = {
+  items: string[];
+  currentIndex: number;
+};
 
-function Banner({ items, currentIndex }: Props) {
+type IndicesProps = {
+  items: string[];
+  currentIndex: number;
+  onCustomClick: (index: number) => void;
+};
+
+function Banner({ items, currentIndex }: BannerProps) {
   return <p>{items[currentIndex]}</p>;
 }
 
-function NextButton({ onCustomClick }) {
+function NextButton({ onCustomClick }: ButtonProps) {
   return <button onClick={onCustomClick}>Next</button>;
 }
 
-function BackButton({ onCustomClick }) {
+function BackButton({ onCustomClick }: ButtonProps) {
   return <button onClick={onCustomClick}>Prev</button>;
 }
 
-function Indices({ items, currentIndex, onCustomClick }: Props) {
+function Indices({ items, currentIndex, onCustomClick }: IndicesProps) {
   const entries = [];
 
   for (let i = 0; i < items.length; i++) {
     if (currentIndex === i) {
       entries.push(
-        <button key={i} className="highlighted" onClick={onCustomClick}>
+        <button
+          key={i}
+          className="highlighted"
+          onClick={() => {
+            onCustomClick(i);
+          }}>
           {i}
         </button>
       );
     } else {
       entries.push(
-        <button key={i} className="" onClick={onCustomClick}>
+        <button
+          key={i}
+          className=""
+          onClick={() => {
+            onCustomClick(i);
+          }}>
           {i}
         </button>
       );
@@ -49,9 +69,8 @@ export default function RotatingBanner({ items }: Props) {
   function handlePrevButton() {
     setCurrentIndex((currentIndex - 1 + items.length) % items.length);
   }
-  function handleIndexClick(event) {
-    setCurrentIndex(Number(event.target.innerText));
-    console.log(event);
+  function handleIndexClick(index: number) {
+    setCurrentIndex(index);
   }
   return (
     <>
